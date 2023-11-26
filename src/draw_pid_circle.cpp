2f5d9dc977fca2071526df56173d9bb3bc3ca803
yongbinlee <yblee001223@naver.com>
2023-11-26 12:35:48 +0000

    FEAT: draw pid circle

--- src/draw_pid_circle.cpp
+++ src/draw_pid_circle.cpp
@@ -2,12 +2,32 @@
 #include <cmath>
 #include "turtlesim/msg/pose.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "error_msgs/msg/error.hpp"
 
 double er_sum = 0;
 double etheta_sum =0;
 double er_prev = 0;
 double etheta_prev =0;
+
+float feed_x(0.0);
+float feed_y(0.0);
+float feed_th(0.0);
+
+double init_x(5.54445);
+double init_y(5.54445);
+double init_th(0.0);
+
+double x(0.0);
+double y(0.0);
+double th(0.0);
+double xdot(0.0);
+double ydot(0.0);
+double xddot(0.0);
+double yddot(0.0);
+
+double t = 0;
+
+double a = 1;
+double b = 1;
 
 class TurtleFbControl : public rclcpp::Node // MODIFY NAME
 {
@@ -26,63 +46,67 @@ public:
     }
 
 private:
+    //PID contorl gain
+    double Kp_r = 1;
+    double Kd_r = 0;
+    double Ki_r = 0;
+
+    double Kp_th = 2;
+    double Kd_th = 0;
+    double Ki_th = 1;
+    
+    //control signal
+    double u_r = 0;
+    double u_theta = 0;
+    //simpler reference point
+    double xr = 9;
+    double yr = 7;
+
+    double theta_r = 0;
+    double e_r = 1;
+    double e_theta = 1;
+
+    double dt = 0.0001;
+    //circle trajectory
 
     void callbackPose(const turtlesim::msg::Pose::SharedPtr pose)
     {   //get Pose msg
         pose_ = *pose.get();
-        RCLCPP_INFO(this->get_logger(), "Received pose: x=%f, y=%f, theta=%f", pose_.x, pose_.y, pose_.theta);
+        //RCLCPP_INFO(this->get_logger(), "Received pose: x=%f, y=%f, theta=%f", pose_.x, pose_.y, pose_.theta);
     }
 
     void controlLoop()
     {
         auto msg = geometry_msgs::msg::Twist();
 
-        //PID control gain
-        double Kp = 1;
-        double Ki = 0;
-        double Kd = 0;
-        double u_r =0;
-        double u_theta =0;
-        //simple reference point
-        double xr = 30;
-        double yr = 30;
-        double theta_r = 0;
-        //circle trajectory
-        
-        //get error
-        auto e_r = sqrt(pow(xr - pose_.x,2) + pow(yr - pose_.y,2));
-        auto e_theta = theta_r - pose_.theta;
+        x = init_x + a * sin(b * t);
+        y = init_y + a * cos(b * t);
+
+        // loss function
+        e_r = sqrt(pow(x - pose_.x, 2) + pow(y - pose_.y, 2));
+        theta_r = atan2((y - pose_.y) , (x - pose_.x));
+        e_theta = theta_r - pose_.theta;
 
         //derive control signal u (with PID control)
-        if (e_r > 0)
-        {
-            double dedt_r = (e_r - er_prev)/0.01;
-            double dedt_theta = (e_theta - etheta_prev)/0.01;
-            er_sum = er_sum + e_r * 0.01;
-            etheta_sum = etheta_sum + e_theta * 0.01;
-            u_r = Ki*e_r + Ki*er_sum + Kd*dedt_r;
-            u_theta = Kp*e_theta + Ki*etheta_sum + Kd*dedt_theta;
-        }
-        else
-        {
-            // target reached!
-            msg.linear.x = 0;
-            msg.angular.z = 0;
-        }
         
+        double dedt_r = (e_r - er_prev) / dt;
+        double dedt_theta = (e_theta - etheta_prev) / dt;
+        er_sum = er_sum + e_r * dt;
+        etheta_sum = etheta_sum + e_theta * dt;
+        u_r = Kp_r * e_r + Ki_r * er_sum + Kd_r * dedt_r;
+        u_theta = Kp_th * e_theta + Ki_th * etheta_sum + Kd_th * dedt_theta;
+        
+
+        RCLCPP_INFO(this->get_logger(), "Received pose: e_r=%f, e_theta=%f",e_r, e_theta);
+        RCLCPP_INFO(this->get_logger(), "Received pose: u_r=%f, u_theta=%f",u_r, u_theta);
+
         msg.linear.x = u_r;
         msg.angular.z = u_theta;
         cmd_vel_publisher_->publish(msg);
         er_prev = e_r;
         etheta_prev = e_theta;
-        // this->publishCmdVel(u_r, u_theta);
-    }
-    void publishCmdVel(double x, double theta)
-    {   //publish control signal u
-        auto msg = geometry_msgs::msg::Twist();
-        msg.linear.x = x;
-        msg.angular.z = theta;
-        cmd_vel_publisher_->publish(msg);
+
+        t = t + dt;
     }
 
     std::string name_;
